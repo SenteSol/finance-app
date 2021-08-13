e190f7dcf850d307b945c5bde31f5fbe07050488
myrdstom <nserekopaul@gmail.com>
2021-08-13 17:48:07 +0300

    Add Payments logic #36

--- src/constants/views/loans.js
+++ src/constants/views/loans.js
@@ -1,16 +1,17 @@
 export const columns = [
   { id: "name", label: "Client", minWidth: 170 },
   { id: "name", label: "Amount", minWidth: 100 },
+  { id: "name", label: "Principle", minWidth: 100 },
   { id: "name", label: "Rate", minWidth: 100 },
   { id: "code", label: "Date Disbursed", minWidth: 100 },
   { id: "name", label: "Months", minWidth: 100 },
-  { id: "name", label: "Interest Amount", minWidth: 100 },
+  { id: "name", label: "Interest Earned", minWidth: 100 },
   { id: "name", label: "Total Owed", minWidth: 100 },
   { id: "name", label: "Comment", minWidth: 100 }
 ];
 
 export const CLIENT_REQUIRED = "Client required";
-export const AMOUNT_REQUIRED = "Address required";
+export const AMOUNT_REQUIRED = "Amount required";
 export const RATE_REQUIRED = "City required";
 export const DATE_DISBURSED_REQUIRED = "Date required";
 
